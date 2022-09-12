e93af59b431198a2acc1ce67e697b83b52cc2985
CallmeAnnie <annabelogbuagu@gmail.com>
2022-09-12 21:13:53 +0100

    update

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - program to print the following file
+ *
+ * Return: programin to print \"Programming is like
+ * building a multilingual puzzle
+ */
+int main(void)
+{
+/*
+*description: print the following text
+*/
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
